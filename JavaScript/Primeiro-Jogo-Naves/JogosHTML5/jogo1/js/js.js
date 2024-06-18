5628aa7ad170b2bf3f3514b772b5411074ed2213
Kaio-code-creator <kaioasousa12@gmail.com>
2024-06-18 16:30:23 -0300

    .

--- JavaScript/Primeiro-Jogo-Naves/JogosHTML5/jogo1/js/js.js
+++ JavaScript/Primeiro-Jogo-Naves/JogosHTML5/jogo1/js/js.js
@@ -12,6 +12,8 @@ function start() { // Inicio da função start()
 //Principais variáveis do jogo
 	
 var jogo = {};
+var velocidade=5;
+var posicaoY = parseInt(Math.random() * 334);
 var TECLA = {
     W: 87,
     S: 83,
@@ -38,6 +40,7 @@ function loop() {
 
 movefundo();
 movejogador();
+moveinimigo1();
 
 } // Fim da função loop()
 
@@ -80,3 +83,17 @@ function movejogador() {
      }
     
      } // fim da função movejogador()
+
+     function moveinimigo1() {
+
+        posicaoX = parseInt($("#inimigo1").css("left"));
+        $("#inimigo1").css("left",posicaoX-velocidade);
+        $("#inimigo1").css("top",posicaoY);
+            
+            if (posicaoX<=0) {
+            posicaoY = parseInt(Math.random() * 334);
+            $("#inimigo1").css("left",694);
+            $("#inimigo1").css("top",posicaoY);
+                
+            }
+    } //Fim da fun��o moveinimigo1()
